66223c6c67a7e2022c5f26405d233d163bc54cbc
Rossco <rbrsmith@gmail.com>
2019-08-13 20:55:26 +0100

    Set up for RSVP connection

--- public/static/local.js
+++ public/static/local.js
@@ -1,11 +1,22 @@
 $(document).ready(function () {
    $("#rsvp-form").submit(function (e) {
       e.preventDefault(); //prevent default form submit
-      var rsvpFormData = $('#rsvp-form').serializeArray();
+      var rsvpFormData = $('#rsvp-form').serializeArray().reduce(function (obj, item) {
+         obj[item.name] = item.value;
+         return obj;
+      }, {});
       console.log(rsvpFormData);
-      $.post("localhost:8080", rsvpFormData, function(data) {
+
+      $.ajax({
+         type: 'POST',
+         url: 'http://localhost:8080',
+         contentType: 'application/json; charset=utf-8',
+         dataType: 'html',
+         data: JSON.stringify(rsvpFormData)
+      }).done(function (data) {
+         console.log("done");
          alert(data);
+           $( "#rsvp-form" ).html("Thank you " + data);
       });
    });
-
 });
--- routes/status.js
+++ routes/status.js
@@ -2,6 +2,29 @@ var express = require('express');
 var router = express.Router();
 var ps = require('ps-node');
 
+function checkForRSVPSpring(mongoStatus, res) {
+
+    ps.lookup({
+        command: 'java',
+        arguments: 'spring-boot-0.1.0.jar',
+    }, function(err, resultList ) {
+        if (err) {
+            throw new Error(err);
+        }
+        var springStatus = "DOWN";
+        resultList.forEach(function (process) {
+            if (process) {
+                console.log('PID: %s, COMMAND: %s, ARGUMENTS: %s', process.pid, process.command, process.arguments);
+                springStatus = "UP";
+            }
+        });
+
+
+        res.render("statusPage", {mongo: mongoStatus, spring: springStatus});
+    });
+};
+
+
 /* GET home page. */
 router.get("/", function(req,res) {
 
@@ -19,10 +42,12 @@ router.get("/", function(req,res) {
             }
         });
 
-        res.render("statusPage", {mongo: mongoStatus});
+        checkForRSVPSpring(mongoStatus, res);
+
     });
 
 
 });
+
 
 module.exports = router;
