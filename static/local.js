26f02f23757f4a12e2dce545c0e3c1a0ae0a0e79
Rossco <rbrsmith@gamil.com>
2019-08-24 13:16:46 +0100

    New Sections

--- static/local.js
+++ static/local.js
@@ -1,4 +1,22 @@
-$(document).on("load", function() {
-   alert("ready");
+$(document).ready(function () {
+   $("#rsvp-form").submit(function (e) {
+      e.preventDefault(); //prevent default form submit
+      var rsvpFormData = $('#rsvp-form').serializeArray().reduce(function (obj, item) {
+         obj[item.name] = item.value;
+         return obj;
+      }, {});
+      console.log(rsvpFormData);
 
+      $.ajax({
+         type: 'POST',
+         url: 'http://rosolynwedding.com:8080',
+         contentType: 'application/json; charset=utf-8',
+         dataType: 'html',
+         data: JSON.stringify(rsvpFormData)
+      }).done(function (data) {
+         console.log("done");
+         alert(data);
+           $( "#rsvp-form" ).html("Thank you " + data);
+      });
+   });
 });
