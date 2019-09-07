750beac8835b09cc4903643a9aae3fc27450ba0a
Rossco <rbrsmith@gamil.com>
2019-09-07 22:09:54 +0100

    Password change and hash

--- routes/index.js
+++ routes/index.js
@@ -1,40 +1,67 @@
 var express = require('express');
+const bcrypt = require('bcrypt');
 var router = express.Router();
+
+const pwdHash = '$2b$10$LdDSPZpMeaSDZA8XL4zzQOmz00hQgmqQcpnU5oK24Q8uqNtyQQjMm';
 
 /* GET home page. */
 router.post('/', function(req, res, next) {
+
+
   if(typeof req.body.pwd !== 'undefined') {
-    req.session.pwd = req.body.pwd;
+
+      req.session.pwd = req.body.pwd;
+
+      bcrypt.compare(req.session.pwd, pwdHash, function(err, result) {
+        if(result) {
+          console.log("Getting session name: " + req.session.name);
+          const name = (req.session.name) ? req.session.name : undefined;
+
+          //   res.sendFile("index.html", { root: './views/' });
+          res.render("indexPage", {name: name});
+
+        } else {
+
+          console.log("Sending error");
+          console.log(err);
+          res.sendFile("error.html", { root: './views/' });
+        }
+
+
+
+      // if(typeof req.session.pwd !== 'undefined' &&
+      //     req.session.pwd === pwdHash) {
+      //
+      //   console.log("Getting session name: " + req.session.name);
+      //   const name = (req.session.name) ? req.session.name : undefined;
+      //
+      //   //   res.sendFile("index.html", { root: './views/' });
+      //   res.render("indexPage", {name: name});
+      // } else {
+      //   console.log("Sending error");
+      //   res.sendFile("error.html", { root: './views/' });
+      // }
+    });
+
   }
 
-  if(typeof req.session.pwd !== 'undefined' &&
-      req.session.pwd === 'foobar') {
-
-      console.log("Getting session name: " + req.session.name);
-      const name = (req.session.name) ? req.session.name : undefined;
-
-   //   res.sendFile("index.html", { root: './views/' });
-      res.render("indexPage", {name: name});
-  } else {
-    console.log("Sending error");
-      res.sendFile("error.html", { root: './views/' });
-  }
 });
 
 router.get('/', function(req, res, next) {
-  if(typeof req.session.pwd !== 'undefined' &&
-      req.session.pwd === 'foobar') {
+  bcrypt.compare(req.session.pwd, pwdHash, function(err, result) {
+    if (result) {
 
-    console.log("Getting session name: " + req.session.name);
+      console.log("Getting session name: " + req.session.name);
 
-    const name = (req.session.name) ? req.session.name : undefined;
+      const name = (req.session.name) ? req.session.name : undefined;
 
-    //res.sendFile("index.html", { root: './views/' });
-    res.render("indexPage", {name: name});
-  } else {
-    console.log("Sending error");
-    res.sendFile("error.html", { root: './views/' });
-  }
+      //res.sendFile("index.html", { root: './views/' });
+      res.render("indexPage", {name: name});
+    } else {
+      console.log("Sending error");
+      res.sendFile("error.html", {root: './views/'});
+    }
+  });
 
 });
 
--- routes/login.js
+++ routes/login.js
@@ -1,24 +1,28 @@
 var express = require('express');
+const bcrypt = require('bcrypt');
 var router = express.Router();
+
+const pwdHash = '$2b$10$LdDSPZpMeaSDZA8XL4zzQOmz00hQgmqQcpnU5oK24Q8uqNtyQQjMm';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  if(typeof req.session.pwd !== 'undefined' &&
-    req.session.pwd === 'foobar') {
+  bcrypt.compare(req.session.pwd, pwdHash, function(err, result) {
+    if (result) {
 
-    console.log("Getting session name: " + req.session.name);
+      console.log("Getting session name: " + req.session.name);
 
-    const name = (req.session.name) ? req.session.name : undefined;
+      const name = (req.session.name) ? req.session.name : undefined;
 
-    //res.sendFile("index.html", { root: './views/' });
-    res.render("indexPage", {name: name});
+      //res.sendFile("index.html", { root: './views/' });
+      res.render("indexPage", {name: name});
 
 //    res.sendFile("index.html", { root: './views/' });
-  } else {
-    //res.render('index', { title: 'Express' });
-    res.sendFile("login.html", {root: "./views/"});
-  }
+    } else {
+      //res.render('index', { title: 'Express' });
+      res.sendFile("login.html", {root: "./views/"});
+    }
+  });
 });
 
 module.exports = router;
